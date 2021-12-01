87d406ad31416ccde2fcc21f766dbbcae4981e11
Charly98cma <1998cma@gmail.com>
2021-12-01 22:00:44 +0100

    Day 01 :snake::star::star: - More efficient, only lists.

--- 2021/day01/day01.py
+++ 2021/day01/day01.py
@@ -1,8 +1,4 @@
-import sys, functools
-
-def parse_line(l): return list(map(list, zip(l, l[1:])))
-def parse_line_2(l): return list(map(sum, zip(l, l[1:], l[2:])))
-
-in_list = [int(x) for x in sys.stdin.read().split("\n")[:-1]]
-print(functools.reduce(lambda acc, l: acc+1 if l[1] > l[0] else acc, parse_line(in_list), 0))
-print(functools.reduce(lambda acc, l: acc+1 if l[1] > l[0] else acc, parse_line(parse_line_2(in_list)), 0))
+from sys import stdin
+in_list = [int(x) for x in stdin.read().split("\n")[:-1]]
+print(len([0 for i in range(len(in_list)-1) if in_list[i] < in_list[i+1]]))
+print(len([0 for i in range(len(in_list)-1) if sum(in_list[i:i+3]) < sum(in_list[i+1: i+4])]))

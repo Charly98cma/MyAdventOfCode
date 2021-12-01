5ad526fbeb2c70686026858c3c7689c2520962b3
Charly98cma <1998cma@gmail.com>
2021-12-01 11:41:58 +0100

    Day 01 :snake::star::star: - But with more functional

--- 2021/day01/day01.py
+++ 2021/day01/day01.py
@@ -1,5 +1,8 @@
-from sys import stdin
+import sys, functools
+
 def parse_line(l): return list(map(list, zip(l, l[1:])))
-in_list = [int(x) for x in stdin.read().split("\n")[:-1]]
-print(len([0 for x, y in parse_line(in_list) if y > x]))
-print(len([0 for x, y in parse_line(list(map(sum, zip(in_list, in_list[1:], in_list[2:])))) if y > x]))
+def parse_line_2(l): return list(map(sum, zip(l, l[1:], l[2:])))
+
+in_list = [int(x) for x in sys.stdin.read().split("\n")[:-1]]
+print(functools.reduce(lambda acc, l: acc+1 if l[1] > l[0] else acc, parse_line(in_list), 0))
+print(functools.reduce(lambda acc, l: acc+1 if l[1] > l[0] else acc, parse_line(parse_line_2(in_list)), 0))

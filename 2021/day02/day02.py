8641b19506fe5dd52e13b4946010b0a0c5993c16
Charly98cma <1998cma@gmail.com>
2021-12-02 18:17:44 +0100

    Day 02 :snake::star::star:

--- 2021/day02/day02.py
+++ 2021/day02/day02.py
@@ -4,6 +4,7 @@ from sys import stdin
 stats_1st = {"forward": 0, "depth": 0}
 stats_2nd = {"forward": 0, "depth": 0, "aim": 0}
 
+
 def main():
     for line in stdin:
         mov, n_chg = line.strip().split(" ")
@@ -12,12 +13,9 @@ def main():
             stats_1st["forward"] += n_chg
             stats_2nd["forward"] += n_chg
             stats_2nd["depth"] += stats_2nd["aim"] * n_chg
-        elif mov == "up":
-            stats_1st["depth"] -= n_chg
-            stats_2nd["aim"] -= n_chg
         else:
-            stats_1st["depth"] += n_chg
-            stats_2nd["aim"] += n_chg
+            stats_1st["depth"] += n_chg if mov == "down" else -n_chg
+            stats_2nd["aim"] += n_chg if mov == "down" else -n_chg
     print("First star : {}".format(stats_1st["forward"] * stats_1st["depth"]))
     print("Second star : {}".format(stats_2nd["forward"] * stats_2nd["depth"]))
 
